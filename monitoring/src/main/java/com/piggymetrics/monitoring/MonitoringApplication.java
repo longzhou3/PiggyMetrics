27e19c95963d36078cbb16a379ee8541889a4d2c
lz <lz@qq.com>
2018-07-08 23:38:34 +0800

    版本更新 admin ui增加

--- monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/piggymetrics/monitoring/MonitoringApplication.java
@@ -1,5 +1,6 @@
 package com.piggymetrics.monitoring;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -8,6 +9,7 @@ import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 @SpringBootApplication
 @EnableTurbineStream
 @EnableHystrixDashboard
+@EnableAdminServer
 public class MonitoringApplication {
 
 	public static void main(String[] args) {
